1156e3c48bcf70f2325e14b1bd96608303f5ca62
patches9119 <pbsanders1@gmail.com>
2020-08-29 18:12:30 -0700

    first working prototype

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# README-generator
+# ** Readme Generator **
+
+ ## Description 
+No description here yet
+
+ ## Table of Contents 
+### -test1
+### -test2
+### -test3
+
+ ## Instalation 
+Here is how to install the app
+
+ ## Usage 
+Here is how to use the app
+
+ ## License 
+MIT
+
+ ## Contributions 
+none
+
+ ## Tests 
+N/A
+
+ ## Questions 
+N/A
---
+++ bin/custom.js
@@ -0,0 +1,140 @@
+#!/usr/bin/env node
+
+var inquirer = require('inquirer');
+
+inquirer
+  .prompt([{
+      type: "input",
+      message: "What is the title of the repository?",
+      name: "title"
+    },
+    {
+      type: "input",
+      message: "What desription would you like to add to the repository?",
+      name: "description"
+    },
+    {
+      type: "input",
+      message: "What elements would you like to include in the table of contents?(seperate each element with an amprisand)",
+      name: "toc"
+    },
+    {
+      type: "input",
+      message: "What would you like in the installation section?",
+      name: "installation"
+    },
+    {
+      type: "input",
+      message: "What would you like in the usage section?",
+      name: "usage"
+    },
+    {
+      type: "input",
+      message: "What licenses are included in this repository?",
+      name: "license"
+    },
+    {
+      type: "input",
+      message: "What would you like to add in the contributing section?",
+      name: "contributing"
+    },
+    {
+      type: "input",
+      message: "What would you like in the tests section?",
+      name: "tests"
+    },
+    {
+      type: "input",
+      message: "What would you like in the questions section?",
+      name: "questions"
+    },
+  ])
+  .then(answers => {
+    var fs = require("fs");
+
+    const title = answers.title;
+    const description = answers.description;
+    const toc = answers.toc;
+    const installation = answers.installation;
+    const usage = answers.usage;
+    const license = answers.license;
+    const contributing = answers.contributing;
+    const tests = answers.tests;
+    const questions = answers.questions;
+    //make sure to change when pushing changes
+    fs.writeFile("README.md", buildTitle(title) + "\n", function (err) {
+      if (err) {
+        console.log(err);
+      }
+
+      fs.appendFile("README.md", "\n ## Description \n" + description + "\n", function (err) {
+        if (err) {
+          console.log(err);
+        }
+
+        fs.appendFile("README.md", "\n ## Table of Contents \n" + buildTOC(toc) + "\n", function (err) {
+          if (err) {
+            console.log(err);
+          }
+
+          fs.appendFile("README.md", "\n ## Instalation \n" + installation + "\n", function (err) {
+            if (err) {
+              console.log(err);
+            }
+
+            fs.appendFile("README.md", "\n ## Usage \n" + usage + "\n", function (err) {
+              if (err) {
+                console.log(err);
+              }
+
+              fs.appendFile("README.md", "\n ## License \n" + license + "\n", function (err) {
+                if (err) {
+                  console.log(err);
+                }
+
+                fs.appendFile("README.md", "\n ## Contributions \n" + contributing + "\n", function (err) {
+                  if (err) {
+                    console.log(err);
+                  }
+
+                  fs.appendFile("README.md", "\n ## Tests \n" + tests + "\n", function (err) {
+                    if (err) {
+                      console.log(err);
+                    }
+
+                    fs.appendFile("README.md", "\n ## Questions \n" + questions + "\n", function (err) {
+                      if (err) {
+                        console.log(err);
+                      }
+                    })
+                  })
+                })
+              })
+            })
+          })
+        })
+      })
+    })
+
+
+
+
+
+  })
+  .catch(error => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
+
+function buildTitle(title) {
+  const newtitle = "# ** " + title + " **";
+  return newtitle;
+}
+
+function buildTOC(toc) {
+  const newTOC = "### -" + toc.replace(/&/g, '\n### -');
+  return newTOC;
+}
